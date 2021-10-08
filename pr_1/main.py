ac4cd05594115f111a0869ea20922ed0dded048a
AlexEra <quietIsaac@ya.ru>
2021-10-08 23:33:28 +0300

    v0.2

--- pr_1/main.py
+++ pr_1/main.py
@@ -1,63 +1,68 @@
-import numpy as np
-
-
-def extract_data():
-    with open('agaricus-lepiota.data', 'r') as f:
-        tmp = f.read().split('\n')
-    data = list()
-    p = list()
-    e = list()
-    for i in range(len(tmp)):
-        data.append(tmp[i].split(','))
-        if data[i][0] == 'p':
-            p.append(data[i])
-        else:
-            e.append(data[i])
-    return data, p, e
-
-
 def write_to_file(data, file_name):
     with open(file_name, 'w') as f:
         for i in data:
             f.writelines(str(i) + '\n')
 
 
-def intersect(a_1, a_2):
-    # TODO: add flag of empty intersection
-    tmp = list()
-    for i, j in zip(a_1, a_2):
-        if i == j and i != '0' and j != '0':
-            tmp.append(i)
-        else:
-            tmp.append('0')
+def extract_data() -> list:
+    with open('agaricus-lepiota.data', 'r') as f:
+        tmp = f.read().split('\n')
     return tmp
 
 
-def equal_hyp(a_0, a_1):  # TODO: check for equal of hypothesis!
-    # была ли ранее гипотеза? Если была, то добавляем 1 к родителям, если нет, то добавляем к гипотезам
-    if a_0 == a_1 and a_0 != '0' and a_1 != '0':
-        return a_1
+def form_dictionaries(f_data: list) -> (dict, dict, list, list):
+    tmp_p, tmp_e = list(), list()
+    p, e = dict(), dict()
+    pp, ee = 1, 1
+    for i in range(len(f_data)):
+        data = f_data[i].replace(',', '')
+        if data[0] == 'p':
+            p[data] = [pp]
+            tmp_p.append(data)
+            pp += 1
+        else:
+            e[data] = [ee]
+            tmp_e.append(data)
+            ee += 1
+    return p, e, tmp_p, tmp_e
+
+
+def intersect(a_1: str, a_2: str) -> (str, bool):
+    tmp = ''
+    flag = False
+    for i, j in zip(a_1, a_2):
+        if i == j and i != '0' and j != '0':
+            tmp += i
+            flag = True
+        else:
+            tmp += '0'
+    return tmp, flag
+
+
+def intersection(dct: dict, key_0: str, key_1: str) -> (dict, bool):
+    res, flg = intersect(key_0, key_1)
+    if not flg:
+        return {}, flg
     else:
-        return '0'
+        return {res: dct[key_0] + dct[key_1]}, flg
+
+
+def check_hypothesis(hypotheses: dict, hyp_to_check: dict) -> dict:
+    # TODO: проверка эквивалетности гипотез
+    # TODO: нужно сравнить с гипотезами выше, к множеству родителей добавляется новый родитель, если есть похожие
+    # была ли ранее гипотеза? Если была, то добавляем 1 к родителям, если нет, то добавляем к гипотезам
+    # ВАЖНО! Списки с номерами родителей нужно предварительно отсортировать!
+    pass
 
 
 # Алгоритм "каждый с каждым"
-# TODO: массив примеров: [примеры и родители], + и -
-# в родители записывается номер индекса (глобальный?)
-# нужно сравнить с гипотезами выше, к множеству родителей добавляется новый родитель, если есть похожие -> equal_hyp
-
-
-# проще сделать так: [[пример: str], [родитель: str]], суммируем строки, так проще
-# родитель будет объявляться с длиной, соответствующей числу (+)-примеров и (-)-примеров, индексы независимы!
-# это удобно для сравнения
 if __name__ == '__main__':
-    #  К следующему разу получить пересечение первых 10 пересечений
-    from_file, poisonous, edible = extract_data()
+    #  TODO: К следующему разу получить пересечение первых 10 пересечений
+    from_file = extract_data()
+    poisonous, edible, list_p, list_e = form_dictionaries(from_file)
     """
     print(f'amount of edible mushrooms: {len(edible)}')
     print(f'amount of poisonous mushrooms: {len(poisonous)}')
     """
-
-    # parents = np.zeros((len(from_file), len(from_file[0])))  # not need
-    parents = [[i+1] for i in range(len(from_file))]
-    print(intersect(from_file[2], from_file[5]))
+    del from_file
+    print(intersection(poisonous, list_p[0], list_p[1]))
