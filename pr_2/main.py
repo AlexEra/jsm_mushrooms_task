0f7b4a7ece9eafc0ff8c18b77f9381aa4329cc39
AlexEra <quietIsaac@ya.ru>
2021-11-15 19:51:14 +0300

    v0.5

--- pr_2/main.py
+++ pr_2/main.py
@@ -75,28 +75,28 @@ def sum_of_parents(p_0: list, p_1: list) -> list:
 
 
 def add_h(example: list, hpts: list, u_set: list) -> list:
+    # TODO: check the logic of this program with debugger
     for y in hpts:
-        if nesting(y[0], example[0]):  # если пример вкладывается в гипотезу
+        if nesting(example[0], y[0]):  # если гипотеза вкладывается в пример
             hpts[hpts.index(y)][1] = sum_of_parents(y[1], example[1])
         else:
             break_flg = False
             z, parents_of_z, intersect_flg = intersect(y, example)
 
             """ проверка на относительную каноничность """
-            for v in u_set[0:u_set.index(example)]:
-                if v_is_parent_of_y(v[1], y[1], u_set.index(v), hpts.index(y)):  # если v является родителем y
+            # for v in u_set[0:u_set.index(example)]:
+            for v in u_set[0:u_set.index(example):-1]:
+                # если v является родителем y ИЛИ z вкладывается в v
+                if v[1][0] in y[1] or nesting(v[0], z):
                     break_flg = True
                     break
-                else:
-                    if nesting(v[0], z):  # если z вкладывается в v
-                        break_flg = True
-                        break
             if not break_flg:
                 if intersect_flg:
                     hpts.append([z, parents_of_z])  # merge_hpts_with_z
 
     """ проверка на асболютную каноничность """
-    for v in u_set[0:u_set.index(example)]:  # TODO: уточнить, проверка ниже проваливается всегда
+    # for v in u_set[0:u_set.index(example)]:
+    for v in u_set[0:u_set.index(example):-1]:
         if nesting(v[0], example[0]):  # если пример вкладывается хотя бы в один из предыдущих примеров
             return hpts
 
@@ -112,7 +112,7 @@ if __name__ == '__main__':
     hypotheses_n = list()
 
     """ основной цикл по (-)-примерам """
-    right_lim = 10
+    right_lim = 5
     for x in poisonous[0:right_lim]:
         hypotheses_n = add_h(x.copy(), hypotheses_n, poisonous[0:right_lim])
     write_to_file(hypotheses_n, 'res')
